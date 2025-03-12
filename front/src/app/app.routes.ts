99037834e00e78f66e182c4bd280fdb1393ae758
FranOrteg <ortegfran@gmail.com>
2025-03-12 17:03:48 +0100

    Get projets Front

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FileBrowserComponent } from './components/file-browser/file-browser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', pathMatch: 'full', component: FileBrowserComponent },
+    { path: 'fileBrowser', component: FileBrowserComponent },
+    { path: '**', redirectTo: '' }
+];
--- front/src/app/components/file-browser/file-browser.component.ts
+++ front/src/app/components/file-browser/file-browser.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccService } from '../../services/acc.service';
 
 @Component({
   selector: 'app-file-browser',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './file-browser.component.html',
   styleUrl: './file-browser.component.css'
 })
-export class FileBrowserComponent {
+export class FileBrowserComponent implements OnInit {
 
+  arrProjects: any[] = [];
+
+  constructor(private accService: AccService) { }
+
+  async ngOnInit(){
+    const response = await this.accService.getProjects
+    ('b.1bb899d4-8dd4-42d8-aefd-6c0e35acd825');
+
+    this.arrProjects = response;
+    console.log(this.arrProjects);
+  }
 }
