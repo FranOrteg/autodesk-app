5d514a4cf94076fd516530fa1bab32c422dbdf4f
FranOrteg <ortegfran@gmail.com>
2025-03-24 15:39:36 +0100

    .rvt Properties

--- backend/models/properties.model.js
+++ backend/models/properties.model.js
@@ -4,8 +4,26 @@ const axios = require('axios');
 /**
  * Obtiene el `metadata` del archivo Revit en Model Derivative API.
  */
+
 async function getMetadata(accessToken, urn) {
     const url = `https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/metadata`;
+
+    const { data } = await axios.get(url, {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return data;
+}
+
+/**
+ * Obtiene el estado del modelo para saber si está completamente procesado.
+ */
+
+async function getModelStatus(accessToken, urn) {
+    const url = `https://developer.api.autodesk.com/modelderivative/v2/designdata/${urn}/manifest`;
 
     const { data } = await axios.get(url, {
         headers: {
@@ -42,18 +60,28 @@ async function getProperties(accessToken, urn, guid) {
 
 async function extractRevitProperties(accessToken, urn){
     try {
-        // 1. Obtener metadata del modelo
+        
+        // 1. Verificar si el modelo está completamente procesado
+        const manifest = await getModelStatus(accessToken, urn);
+        if (manifest.progress !== "complete") {
+            throw new Error(`El modelo aún no está completamente procesado. Progreso: ${manifest.progress}`);
+        }
+
+        // 2. Obtener metadata del modelo
         const metadata = await getMetadata(accessToken, urn);
         
         if(!metadata.data.metadata || metadata.data.metadata.length === 0){
             throw new Error('No metadata found for the model.');
         }
 
-        // 2. Extraer el primer GUID disponible (puede haber varios)
-        const guid = metadata.data.metadata[0].guid;
+        // 3. Extraer el primer GUID disponible (puede haber varios)
+        let guid = metadata.data.metadata.find(view => view.isMasterView)?.guid || metadata.data.metadata[0].guid;
 
-        // 3. Obtener propiedades del modelo
+        // 4. Obtener propiedades del modelo
         const propertiesData = await getProperties(accessToken, urn, guid);
+        if (!propertiesData.data.collection) {
+            throw new Error("No se encontraron propiedades en la respuesta de la API.");
+        }
 
         // 4. Formatear las propiedades en un objeto organizado por objectId
         const propertiesMap = {};
@@ -70,5 +98,6 @@ async function extractRevitProperties(accessToken, urn){
 
 module.exports = {
     extractRevitProperties,
+    getModelStatus,
     getMetadata
 }
