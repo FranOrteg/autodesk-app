abd9b4e1f63b90d642f170b37b238628e2c5f170
FranOrteg <ortegfran@gmail.com>
2025-04-01 15:15:13 +0200

    app.js Aumento de limite de transferencia de datos

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
+
 const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+// Aumenta el límite de tamaño permitido (por ejemplo, 50MB)
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Aumenta el límite de tamaño permitido (por ejemplo, 50MB)
-app.use(bodyParser.json({ limit: '150mb' }));
-app.use(bodyParser.urlencoded({ limit: '150mb', extended: true }));
-
-app.use(express.json({ limit: '150mb' }));
-app.use(express.urlencoded({ limit: '150mb', extended: true }));
 
 // Rutas
 app.use('/api', require('./routes/api'));
