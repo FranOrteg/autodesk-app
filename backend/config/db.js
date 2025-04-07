4e5696099da4aa17572d6149c6129d842b583d2d
FranOrteg <ortegfran@gmail.com>
2025-04-07 11:15:29 +0200

    Generate Prod Pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,22 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+/* DEV POOL */
+const poolDev = mysql.createPool({
+    host: process.env.DB_HOST_DEV,
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    port: process.env.DB_PORT_DEV,
+    database: process.env.DB_NAME_DEV
 });
-global.db = pool.promise();
+
+/* PROD POOL */
+const pool = mysql.createPool({
+    host: process.env.DB_HOST_DEV,
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    port: process.env.DB_PORT_DEV,
+    database: process.env.DB_NAME_DEV
+});
+
+/* CHANGE POOL FOR PROD */
+global.db = poolDev.promise();
